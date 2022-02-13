aacf1263bc673fa5475a0b8e412a2b686602dcb6
GlenDu <glen_llaci@yahoo.com>
2022-02-13 21:56:29 +0100

    read and parser

---
+++ lib/read_json.rb
@@ -0,0 +1,16 @@
+require 'json'
+require 'byebug'
+
+class JsonFile
+
+  def parse_json
+    JSON.parse(File.read file_path)
+  end
+
+  private
+
+  def file_path
+    './data/users.json'
+  end
+
+end
---
+++ main.rb
@@ -0,0 +1,2 @@
+require_relative './lib/intermedian'
+require_relative '.lib/read_json'
