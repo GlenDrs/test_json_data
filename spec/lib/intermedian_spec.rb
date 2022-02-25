2f255f0e908f6a1bf16a0df3fe2e12ef7b273128
GlenDu <glen_llaci@yahoo.com>
2022-02-25 19:47:08 +0100

    first test of Intermedian

--- spec/lib/intermedian_spec.rb
+++ spec/lib/intermedian_spec.rb
@@ -1,1 +1,8 @@
-require '../lib/intermedian'
+require_relative '../../lib/intermedian'
+
+RSpec.describe Intermedian do
+  let (:heads) { described_class.new.creating_heads }
+  id do
+    expect(heads).to eq [id,email,tags,profiles.facebook.id,profiles.facebook.picture,profiles.twitter.id,profiles.twitter.picture]
+  end
+end
