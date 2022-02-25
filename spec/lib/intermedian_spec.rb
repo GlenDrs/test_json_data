e0e91867f0d503815f5d3c67bfe851f22c97d636
GlenDu <glen_llaci@yahoo.com>
2022-02-25 19:58:01 +0100

    Intermedian test 2

--- spec/lib/intermedian_spec.rb
+++ spec/lib/intermedian_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Intermedian do
   id do
     expect(heads).to eq [id,email,tags,profiles.facebook.id,profiles.facebook.picture,profiles.twitter.id,profiles.twitter.picture]
   end
+
+  context "creating array"
+  id do
+    let (:return_array) { described_class.new.nested_hash_2_arr }
+    expect(return_array).to include
+    (
+    [0,
+    "colleengriffith@quintity.com",
+    ["consectetur", "quis"],
+    0,
+    "//fbcdn.com/a2244bc1-b10c-4d91-9ce8-184337c6b898.jpg",
+    0,
+    "//twcdn.com/ad9e8cd3-3133-423e-8bbf-0602e4048c22.jpg"]
+    )
+  end
 end
