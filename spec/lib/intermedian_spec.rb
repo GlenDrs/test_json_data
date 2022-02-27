6ea003404189195c1c58180b85579babe6a0c7fc
GlenDu <glen_llaci@yahoo.com>
2022-02-27 16:48:43 +0100

    last test and modifications

--- spec/lib/intermedian_spec.rb
+++ spec/lib/intermedian_spec.rb
@@ -7,21 +7,28 @@ RSpec.describe Intermedian do
     expect(intermedian.creating_heads).to eq ["id", "email", "tags", "profiles.facebook.id", "profiles.facebook.picture", "profiles.twitter.id", "profiles.twitter.picture"]
   end
 
-  it "#nested_hash_2_arr" do
-    expect(intermedian.nested_hash_2_arr).to be_a Array
-  end
+  describe '#nested_hash_2_arr' do
+    it "Check Array" do
+      expect(intermedian.nested_hash_2_arr).to be_a Array
+    end
 
-  context "Verify Array"
-  it do
-    expect(intermedian.nested_hash_2_arr).to include
-    (
-    [0,
-    "colleengriffith@quintity.com",
-    ["consectetur", "quis"],
-    0,
-    "//fbcdn.com/a2244bc1-b10c-4d91-9ce8-184337c6b898.jpg",
-    0,
-    "//twcdn.com/ad9e8cd3-3133-423e-8bbf-0602e4048c22.jpg"]
-    )
+    context "Array not void"
+    it do
+      expect(intermedian.nested_hash_2_arr).not_to be_empty
+    end
+
+    context "Verify Array"
+    it do
+      expect(intermedian.nested_hash_2_arr).to include
+      (
+      [0,
+      "colleengriffith@quintity.com",
+      ["consectetur", "quis"],
+      0,
+      "//fbcdn.com/a2244bc1-b10c-4d91-9ce8-184337c6b898.jpg",
+      0,
+      "//twcdn.com/ad9e8cd3-3133-423e-8bbf-0602e4048c22.jpg"]
+      )
+    end
   end
 end
